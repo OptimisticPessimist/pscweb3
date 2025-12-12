1b807b4afc57ae415b9b46894a41168b9761f7fb
OptimisticPessimist <>
2025-12-13 02:14:27 +0900

    feat: マニュアルを5言語対応（日英韓中繁）

---
+++ frontend/src/pages/manualContent/index.ts
@@ -0,0 +1,5 @@
+export { manualJa } from './ja';
+export { manualEn } from './en';
+export { manualKo } from './ko';
+export { manualZhHans } from './zh-Hans';
+export { manualZhHant } from './zh-Hant';
